f9834732cf63a0b1b7fa48f101797768accbcf98
Kaan Kölköy <kaanklky@gmail.com>
2023-05-05 00:24:07 +0300

    change mongodb user detection

--- snippets/prod_data_disk_initialization/init_data_disk.sh
+++ snippets/prod_data_disk_initialization/init_data_disk.sh
@@ -1,11 +1,12 @@
-MONGO_USER=$(grep mongo /etc/passwd | head -1 | awk -F':' '{print $1}')
+MONGODB_SERVICE_FILE=$(systemctl status mongod|grep loaded|awk -F'(' '{print $2}'|awk -F';' '{print $1}')
+MONGODB_USER=$(grep 'User=' "${MONGODB_SERVICE_FILE}"|awk -F'=' '{print $2}')
 
 printf "o\nn\np\n1\n\n\nw\n" | sudo fdisk /dev/sdb
 
 mkfs.xfs /dev/sdb1
 mkdir /data
 mount /dev/sdb1 /data
-chown -R ${MONGO_USER}:${MONGO_USER} /data
+chown -R ${MONGODB_USER}:${MONGODB_USER} /data
 
 sed -i 's#\/var\/lib\/mongodb#\/data#g' /etc/mongod.conf
 systemctl restart mongod
